177dc19faf2611e5f2b1e3ee43c9ddebd9591cb9
attilajak <41372370+attilajak@users.noreply.github.com>
2022-09-15 19:09:45 +0530

    Add files via upload

---
+++ kube.sh
@@ -0,0 +1,14 @@
+
+alias kgp='kubectl get po' 
+alias kgs='kubectl get svc' 
+alias kgd='kubectl get deployments' 
+alias ka='kubectl apply -f' 
+alias kd='kubectl delete -f' 
+alias kdx='kubectl delete po'
+alias kl='kubectl logs'
+
+
+alias igp='kubectl get po -n istio-system' 
+alias igs='kubectl get svc -n istio-system' 
+alias igd='kubectl get deployments -n istio-system' 
+
